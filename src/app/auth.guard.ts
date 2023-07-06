e951303c8ca43402f46230c15355e8c0f2a7a4b5
swatiamberkar <swatiamberkar29@gmail.com>
2023-07-06 15:22:00 +0530

    update limit

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { SellerService } from './services/seller.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
 
   constructor(
     private sellerService:SellerService
--- src/app/cart-page/cart-page.component.ts
+++ src/app/cart-page/cart-page.component.ts
@@ -8,6 +8,7 @@ import { Router } from '@angular/router';
   templateUrl: './cart-page.component.html',
   styleUrls: ['./cart-page.component.scss']
 })
+
 export class CartPageComponent implements OnInit{
 
   cartData:Cart[] |undefined
